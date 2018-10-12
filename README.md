2e972a43d8eaf1db6a598980338bf80f953fb107
Collin Weyel <18754933+DiamandDragon@users.noreply.github.com>
2018-10-12 21:54:57 +0200

    Added Modes without Submodes

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # ADIF-Parser
 
 Currently there is no support planned for the enumerations ContestID, Credit, Primary Administrative Subdivision, Secondary Administrative Subdivision, U.S. Counties and Sponsored Award.
+Currently there are also no submodes implemented.
---
+++ src/bid/thediamonddragon/adifparser/enums/Mode.java
@@ -0,0 +1,58 @@
+package bid.thediamonddragon.adifparser.enums;
+
+public enum Mode {
+
+    AM("AM"),
+    ARDOP("ARDOP"),
+    ATV("ATV"),
+    C4FM("C4FM"),
+    CHIP("CHIP"),
+    CLO("CLO"),
+    CONTESTI("CONTESTI"),
+    CW("CW"),
+    DIGITALVOICE("DIGITALVOICE"),
+    DOMINO("DOMINO"),
+    DSTAR("DSTAR"),
+    FAX("FAX"),
+    FM("FM"),
+    FSK441("FSK441"),
+    FT8("FT8"),
+    HELL("HELL"),
+    ISCAT("ISCAT"),
+    JT4("JT4"),
+    JT6M("JT6M"),
+    JT9("JT9"),
+    JT44("JT44"),
+    JT65("JT65"),
+    MFSK("MSFK"),
+    MSK144("MSK144"),
+    MT63("MT63"),
+    OLIVIA("OLIVIA"),
+    OPERA("OPERA"),
+    PAC("PAC"),
+    PAX("PAX"),
+    PKT("PKT"),
+    PSK("PSK"),
+    PSK2K("PSK2K"),
+    Q15("Q15"),
+    QRA64("QRA64"),
+    ROS("ROS"),
+    RTTY("RTTY"),
+    RTTYM("RTTYM"),
+    SSB("SSB"),
+    SSTV("SSTV"),
+    T10("T10"),
+    THOR("THOR"),
+    THRB("THRB"),
+    TOR("TOR"),
+    V4("V4"),
+    VOI("VOI"),
+    WINMOR("WINMOR"),
+    WSPR("WSPR");
+
+    private String name;
+
+    Mode(String name){
+        this.name = name;
+    }
+}
