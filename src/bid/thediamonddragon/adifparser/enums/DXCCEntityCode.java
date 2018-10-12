8e4bf1ac23c238ebe467e31fb5fcffc29c3d7a2d
Collin Weyel <18754933+DiamandDragon@users.noreply.github.com>
2018-10-12 21:30:10 +0200

    Added the first 30 DXCC entity codes

---
+++ src/bid/thediamonddragon/adifparser/enums/DXCCEntityCode.java
@@ -0,0 +1,43 @@
+package bid.thediamonddragon.adifparser.enums;
+
+public enum DXCCEntityCode {
+    //TODO: Add remaining codes
+    NULL(0, "None"),
+    ONE(1, "CANADA"),
+    TWO(2, "ABU AIL IS."),
+    THREE(3, "AFGHANISTAN"),
+    FOUR(4, "AGALEGA & ST. BRANDON IS."),
+    FIVE(5, "ALAND IS."),
+    SIX(6, "ALASKA"),
+    SEVEN(7, "ALBANIA"),
+    EIGHT(8, "ALDABRA"),
+    NINE(9, "AMERICAN SAMOA"),
+    TEN(10, "AMSTERDAM & ST. PAUL IS."),
+    ELEVEN(11, "ANDAMAN & NICOBAR IS."),
+    TWELVE(12, "ANGUILLA"),
+    THIRTEEN(13, "ANTARCTICA"),
+    FOURTEEN(14, "ARMENIA"),
+    FIVETEEN(15, "ASIATIC RUSSIA"),
+    SIXTEEN(16, "NEW ZEALAND SUBANTARCTIC ISLANDS"),
+    SEVENTEEN(17, "AVES I."),
+    EIGHTTEEN(18, "AZERBAIJAN"),
+    NINETEEN(19, "BAJO NUEVO"),
+    TWENTY(20, "BAKER & HOWLAND IS."),
+    TWENTYONE(21, "BALEARIC IS."),
+    TWENTYTWO(22, "PALAU"),
+    TWENTYTHREE(23, "BLENHEIM REEF"),
+    TWENTYFOUR(24, "BOUVET"),
+    TWENTYFIVE(25, "BRITISH NORTH BORNEO"),
+    TWENTYSIX(26, "BRITISH SOMALILAND"),
+    TWENTYSEVEN(27, "BELARUS"),
+    TWENTYEIGHT(28, "CANAL ZONE"),
+    TWENTYNINE(29, "CANARY IS.");
+
+    private int code;
+    private String name;
+
+    DXCCEntityCode(int code, String name){
+        this.code = code;
+        this.name= name;
+    }
+}
